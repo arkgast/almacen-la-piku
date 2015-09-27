b7612872d39f12fa8b948428bb8e71bf869eb9ce
Arnold Gandarillas Castillo <a.kltrx@gmail.com>
2015-09-27 13:36:17 -0400

    Fix first bug

--- almacen/settings.py
+++ almacen/settings.py
@@ -127,3 +127,4 @@ DATABASES['default'] = dj_database_url.config()
 DATABASES['default']['ENGINE'] = 'django_postgrespool'
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 ALLOWED_HOSTS = ['*']
+STATIC_ROOT = 'staticfiles'
